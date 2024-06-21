7f9adfa9f9fb6be9b26794275460d0ffd32e2a48
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-06-21 08:21:14 +0200

    chore: Update Docker configuration for server and database

--- server/main.go
+++ server/main.go
@@ -1,12 +1,41 @@
 package main
+
 import (
+    "database/sql"
     "fmt"
+    "log"
     "net/http"
+    "os"
+
+    _ "github.com/lib/pq"
 )
+
 func helloWorld(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, World!")
 }
+
 func main() {
+ 
+    psqlInfo := "host=db port=5432 user=user1 dbname=mydatabase password=password123 sslmode=disable"
+
+    // Open the connection
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
+    defer db.Close()
+
+    // Check the connection
+    err = db.Ping()
+    if err != nil {
+        log.Fatalf("Error connecting to the database: %q", err)
+    }
+
+    fmt.Println("Successfully connected to the database")
+
     http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":8080", nil)
+    fmt.Println("Server starting on port 8080...")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
 }

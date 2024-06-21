4f3b90496d0a0dc6c88dfe301e7b5c9f89524327
Ogesebastien-epitech <sebastien.oge@epitech.eu>
2024-06-21 08:42:07 +0200

    chore: Update database connection configuration with environment variables

--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
     "os"
 
     _ "github.com/lib/pq"
+    "github.com/joho/godotenv"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -15,20 +16,32 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
- 
-    psqlInfo := "host=db port=5432 user=user1 dbname=mydatabase password=password123 sslmode=disable"
+    // Load .env file
+    err := godotenv.Load()
+
+    if err != nil {
+        log.Fatalf("Error loading .env file: %s", err)
+    }
+
+    // Get database credentials from environment variables
+    dbUser := os.Getenv("POSTGRES_USER")
+    dbPassword := os.Getenv("POSTGRES_PASSWORD")
+    dbName := os.Getenv("POSTGRES_DB")
+
+    // Connection string
+    psqlInfo := fmt.Sprintf("host=db port=5432 user=%s dbname=%s password=%s sslmode=disable", dbUser, dbName, dbPassword)
 
     // Open the connection
     db, err := sql.Open("postgres", psqlInfo)
     if err != nil {
-        log.Fatalf("Error opening database: %q", err)
+        log.Fatalf("Error opening database: %s", err)
     }
     defer db.Close()
 
     // Check the connection
     err = db.Ping()
     if err != nil {
-        log.Fatalf("Error connecting to the database: %q", err)
+        log.Fatalf("Error connecting to the database: %s", err)
     }
 
     fmt.Println("Successfully connected to the database")
@@ -36,6 +49,6 @@ func main() {
     http.HandleFunc("/", helloWorld)
     fmt.Println("Server starting on port 8080...")
     if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Printf("Error starting server: %s\n", err)
+        fmt.Printf("Error starting server: %s", err)
     }
 }
